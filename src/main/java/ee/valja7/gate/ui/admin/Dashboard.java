cd971e97ce153d023587d882a95db5eaf29ff336
Valeri Kuzmin <orhdposit@gmail.com>
2016-05-19 20:06:29 +0300

    MobileGate-2 move Login form to navigation bar

--- src/main/java/ee/valja7/gate/ui/admin/Dashboard.java
+++ src/main/java/ee/valja7/gate/ui/admin/Dashboard.java
@@ -2,11 +2,9 @@ package ee.valja7.gate.ui.admin;
 
 import ee.valja7.gate.ui.View;
 
-import java.util.Date;
-
 public class Dashboard extends View {
     @Override
     public void execute() {
-        put("now", new Date());
+
     }
 }
